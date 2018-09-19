f2c1a9cc47771a9157710477155593045f75c3c7
Vishal Bhardwaj <vishalbhardwaj998@gmail.com>
2018-09-19 16:40:57 +0530

    working on..

--- crypto.py
+++ crypto.py
@@ -14,18 +14,32 @@ def getExchangeRates():
 		url="http://data.fixer.io/api/latest?access_key=c47aa2253f59a0e4c58b1202ba6ff451"
 		request=requests.get(url)
 		data=request.json()
-		return data
-		# date=data['date']
-		# rates=data['rates']
+		if data!=False:
+			date=data['date']
+			rates=data['rates']
+			today=getMonth(date)
+			#since the price of Cryptocurrencies is initially given in Dollors
+			inr=rates['INR']
+			usd=rates['USD']
+			#all exchange rates are given in relation to Euro
+			factor=inr/usd
+			factor=float("{0:.4f}".format(factor))
+			print("Dated : "+today)
+			print("Exchange Rate today is : 1 USD = "+str(factor)+" INR")
 	except:
-		return False
+		print("Can't Reach the Destination ...")
+
+def ChooseTimeForChecking():
+	print("Choose Suitable option: \n1. Check Previous 1 hour\n2. Check Previous 24 hours\n3. Check Previous 7 days\n")
+	time=int(input())
+
 
 def getCoinMarketCapAll():
 	#globalURL = "https://api.coinmarketcap.com/v1/global/"
 	tickerURL = "https://api.coinmarketcap.com/v1/ticker/"
 	#Pretty Table to format Output
 	table = pt()
-	table.field_names = ["Name (Symbols)", "Price (in USD)", "Change (1hr)", "Change (24hr)","Change (7days)"]
+	table.field_names = ["Name (Symbols)", "Price (in USD)", "Percentage Change (1hr)", "Percentage Change (24hr)","Percentage Change (7days)"]
 	request = requests.get(tickerURL)
 	data = request.json()
 	for x in data:
@@ -45,11 +59,14 @@ def getCoinMarketCapAll():
 
 def getCoinCapSingle():
 	table=pt()
+	table.field_names = ["Name (Symbols)", "Price (in USD)", "Change (1hr)", "Change (24hr)","Change (7days)"]
+	tickerURL = "https://api.coinmarketcap.com/v1/ticker/"
+	choice=input("Enter Name of cryptocurrency: ")
 	tickerURL += '/'+choice+'/'
 	request = requests.get(tickerURL)
 	data = request.json()
-	ticker = data[0]['symbol']
-	price = data[0]['price_usd']
+	CurrencyName = data[0]['symbol']
+	price_usd = data[0]['price_usd']
 	percent_change_1h = data[0]['percent_change_1h']
 	percent_change_24h = data[0]['percent_change_24h']
 	percent_change_7d = data[0]['percent_change_7d']
@@ -61,37 +78,35 @@ def DriverMenu():
 	driver="yes"
 	while(driver=="yes"):
 		print("Choose Suitable Option : \n1. Get Status of Single Cryptocurrency\n2. Fetch top 100 Cryptocurrencies\n3. Find Best Investment")
-		choice=input()
-		if input==1:
+		choice=int(input())
+		if choice==1:
 			table=getCoinCapSingle()
 			print(table)
-		elif input==2:
+		elif choice==2:
 			table=getCoinMarketCapAll()
 			print(table)
-		elif input==3:
-			pass
+		elif choice==3:
+			ChooseTimeForChecking()
 		else:
 			print("Enter a Valid Input ...")
 		driver=input("Enter 'yes' to continue... ")	
 
 def main():
-	data=getExchangeRates()
-	if data!=False:
-		date=data['date']
-		rates=data['rates']
-		today=getMonth(date)
-		#since the price of Cryptocurrencies is initially given in Dollors
-		inr=rates['INR']
-		usd=rates['USD']
-		#all exchange rates are given in relation to Euro
-		factor=inr/usd
-		factor=float("{0:.4f}".format(factor))
-		print("Dated : "+today)
-		print("Exchange Rate today is : 1 USD = "+str(factor)+" INR")
-		coinCap = getCoinMarketCap()
-		bestInvestment = getBestInvestment(coinCap)
-	else:
-		print("Can't Reach the Destination ...")
+	getExchangeRates()
+	# if data!=False:
+	# 	date=data['date']
+	# 	rates=data['rates']
+	# 	today=getMonth(date)
+	# 	#since the price of Cryptocurrencies is initially given in Dollors
+	# 	inr=rates['INR']
+	# 	usd=rates['USD']
+	# 	#all exchange rates are given in relation to Euro
+	# 	factor=inr/usd
+	# 	factor=float("{0:.4f}".format(factor))
+	# 	print("Dated : "+today)
+	# 	print("Exchange Rate today is : 1 USD = "+str(factor)+" INR")
+	# 	coinCap = getCoinMarketCap()
+	#	bestInvestment = getBestInvestment(coinCap)
 	DriverMenu()
 
 if __name__ == '__main__':
